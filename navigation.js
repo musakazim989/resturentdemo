dfa233fb0a122711b7fd1b4d677f934bf18d3a88
musakazim989 <musakazim989@gmail.com>
2024-03-27 01:28:37 +0600

    feat: added modal into navigation bar inside options 'presentation:'modal' also add function for cartscreen

--- navigation.js
+++ navigation.js
@@ -2,6 +2,7 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {HomeScreen} from './screens/HomeScreen';
 import {ResturantScreen} from './screens/ResturantScreen';
+import {CartScreen} from './screens/CartScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -14,6 +15,11 @@ export function Navigation() {
         }}>
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Resturant" component={ResturantScreen} />
+        <Stack.Screen
+          name="Cart"
+          options={{presentation: 'modal'}}
+          component={CartScreen}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
