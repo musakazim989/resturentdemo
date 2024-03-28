9b5b4979805fcf12de4a1d1685d75dd58139f57d
musakazim989 <musakazim989@gmail.com>
2024-03-28 23:19:26 +0600

    error: API_KEY meta-data google.android.geo.API_KEY

--- screens/DeliveryScreen.js
+++ screens/DeliveryScreen.js
@@ -1,14 +1,32 @@
 import {View, Text} from 'react-native';
 import React from 'react';
-import { featured } from '../constants';
-import { useNavigation } from '@react-navigation/native';
+import {featured} from '../constants';
+import {useNavigation} from '@react-navigation/native';
+import MapView from 'react-native-maps';
+import {Marker} from 'react-native-maps';
 
 export const DeliveryScreen = () => {
-    const resturant= featured.resturants[0]
-    const navigation = useNavigation()
+  const resturant = featured.resturants[0];
+  const navigation = useNavigation();
+  console.log(resturant.lat, resturant.lang);
   return (
     <View className="flex-1 ">
-      <Text>DeliveryScreen</Text>
+      {/* map view */}
+
+      {/* <Text>delivery screen</Text> */}
+
+      <MapView
+        initialRegion={{
+          latitude: resturant.lat,
+          longitude: resturant.lang,
+          // latitudeDelta: 0.0922,
+          // longitudeDelta: 0.0421,
+        }}
+        className="flex-1 ">
+        <Marker
+          coordinate={{latitude: resturant.lat, longitude: resturant.lang}}
+        />
+      </MapView>
     </View>
   );
 };
